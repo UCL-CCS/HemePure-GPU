b63d2951b3f2a5588c5f926598b26d0c7bbc9270
Balint Joo <joob@ornl.gov>
2022-08-26 11:42:31 -0400

    Modified for Crusher  * Seperated out CXX and HIP sources  * HIP Sources compile with CMake HIP Language Extensions  * Avoids needing the -D__HIP_PLATFORM_XXX__ defines in principle.  * Added some checking to top level FullBuild.sh

--- FullBuild.sh
+++ FullBuild.sh
@@ -2,13 +2,35 @@
 ## Compilation/build script for HEMELB
 ## Run from found location
 
-## MODULE loads
-export CC=$(which gcc)
-export CXX=$(which g++)
-export MPI_C_COMPILER=$(which mpicc)
-export MPI_CXX_COMPILER=$(which mpicxx)
+HOST_DOMAIN=`/bin/hostname -d | cut -f1 -d.`
 
-export SOURCE_DIR=/Path_To_GPU_code
+if [ "X${HOST_DOMAIN}" == "Xcrusher" ];
+then
+	echo "Building On Crusher."
+	module load craype-accel-amd-gfx90a   # For GPU Aware MPI
+	module load PrgEnv-amd							  # To use hipcc under CC wrapper
+	module unload rocm										# If rocm is loaded loading amd will complain
+	module load amd												# Load default version of hipcc
+	module load rocm											# Load default version of ROCm
+	module load cmake											# Cmake
+	module unload cray-libsci							# Nuisance we don't need but PrgEnv loads it
+
+	# C/C++ Compilers through Cray Wrappers (why....?)
+	export CC=cc
+	export CXX=CC
+	export MPI_C_COMPILER=cc
+	export MPI_CXX_COMPILER=CC
+	export CUDA_CMAKE_FLAGS="" 
+else
+## MODULE loads
+  export CC=$(which gcc)
+  export CXX=$(which g++)
+  export MPI_C_COMPILER=$(which mpicc)
+  export MPI_CXX_COMPILER=$(which mpicxx)
+  export CUDA_CMAKE_FLAGS="-DCMAKE_CUDA_FLAGS=\"-ccbin g++ -gencode arch=compute_70,code=sm_70 -lineinfo --ptxas-options=-v --disable-warnings\"" 
+fi
+
+export SOURCE_DIR=${PWD}
 
 ## HEMELB build
 # 1) Dependencies
@@ -27,6 +49,8 @@ BuildSource(){
 cd $SOURCE_DIR/src
 rm -rf build
 mkdir build
+
+
 cd build
 cmake  \
   -DCMAKE_CXX_FLAGS="-std=c++11 -g -Wno-narrowing" \
@@ -36,8 +60,8 @@ cmake  \
   -DHEMELB_WALL_INLET_BOUNDARY="NASHZEROTHORDERPRESSURESBB"\
   -DHEMELB_OUTLET_BOUNDARY="NASHZEROTHORDERPRESSUREIOLET"\
   -DHEMELB_WALL_OUTLET_BOUNDARY="NASHZEROTHORDERPRESSURESBB"\
-  -DCMAKE_CUDA_FLAGS="-ccbin g++ -gencode arch=compute_70,code=sm_70 -lineinfo --ptxas-options=-v --disable-warnings" \
-  	$SOURCE_DIR/src
+	${CUDA_CMAKE_FLAGS} \
+ 	$SOURCE_DIR/src
 make -j && echo "Done HemeLB Source"
 cd ../..
 }
