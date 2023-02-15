841bf35447309f8c6e4fa6f86b2a42eef85c811d
Balint Joo <joob@ornl.gov>
2023-02-15 10:45:23 -0500

    Updated Crusher builds for ROCm-5.3.0

--- dep/env.sh
+++ dep/env.sh
@@ -7,3 +7,7 @@ module load cmake                     # Cmake
 module unload cray-libsci             # Nuisance we don't need but PrgEnv loads
 export PATH=${ROCM_PATH}/llvm/bin:${PATH}
 export LD_LIBRARY_PATH=${ROCM_PATH}/llvm/lib:${LD_LIBRARY_PATH}
+
+#module load cray-python/3.9.12.1
+#module use /sw/crusher/amdsw/miperf/modulefiles/rocm-5.1.0
+#module load miperf
