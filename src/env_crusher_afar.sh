9ecdd1f6c8316aca7c554ba4b83f143a19e4caeb
Balint Joo <joob@ornl.gov>
2023-08-18 12:47:59 -0400

    Moved to new Crusher Combo

---
+++ src/env_crusher_afar.sh
@@ -0,0 +1,24 @@
+module load cpe/23.03
+module load PrgEnv-amd/8.4.0
+module load craype-accel-amd-gfx90a   # For GPU Aware MPI
+module load cray-mpich/8.1.26
+module load cray-python
+module load amd/5.4.3
+module load cmake                     # Cmake
+module unload cray-libsci             # Nuisance we don't need but PrgEnv loads
+export LD_LIBRARY_PATH=$CRAY_LD_LIBRARY_PATH:$LD_LIBRARY_PATH
+
+export MPICH_ROOT=/opt/cray/pe/mpich/8.1.26
+export GTL_ROOT=/opt/cray/pe/mpich/8.1.26/gtl/lib
+export MPICH_DIR=${MPICH_ROOT}/ofi/amd/5.0
+export CMAKE_PREFIX_PATH=${ROCM_PATH}/lib/cmake:${CMAKE_PREFIX_PATH}
+
+MPI_CFLAGS="${CRAY_XPMEM_INCLUDE_OPTS} -I${MPICH_DIR}/include "
+MPI_LDFLAGS=" ${CRAY_XPMEM_POST_LINK_OPTS} -lxpmem  -Wl,-rpath=${MPICH_DIR}/lib -L${MPICH_DIR}/lib -lmpi -Wl,-rpath=${GTL_ROOT} -L${GTL_ROOT} -lmpi_gtl_hsa -L${ROCM_PATH}/llvm/lib -Wl,-rpath=${ROCM_PATH}/llvm/lib"
+
+export MPICH_GPU_SUPPORT_ENABLED=1
+export PK_BUILD_TYPE="Release"
+export PATH=${ROCM_PATH}/llvm/bin:$PATH
+
+export LD_LIBRARY_PATH=${ROCM_PATH}/llvm/lib:${ROCM_PATH}/lib:${LD_LIBRARY_PATH}
+ex
